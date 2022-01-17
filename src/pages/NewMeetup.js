56f35c48eca92cce65890cf790ec7ca6ad464f4c
Kamil Szalast <kamilszalast0@gmail.com>
2022-01-17 22:44:51 +0100

    feat: add realtime database on firebase to store added meetups by user

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,25 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useNavigate } from "react-router-dom";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
+  function addMeetupData(meetupData) {
+    fetch(
+      "https://react-getting-started-ac66a-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-type": "application/json" },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupData} />
     </section>
   );
 }
